f7697dffea31cdbe788329ddc858f6c2b0b2cdf0
Aaron Hayman <aaron@flexilesoft.com>
2018-05-07 14:25:24 -0400

    Create jupyter_extensions.sh

---
+++ jupyter_extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+mkdir -p $(jupyter --data-dir)/nbextensions
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+chmod -R go-w vim_binding
+jupyter nbextension enable vim_binding/vim_binding
+jupyter nbextension enable hinterland/hinterland
